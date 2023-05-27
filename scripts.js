a31fade5c07a20bcd31135d303cca4fb6a28479e
Jemrel Ricky P. Mangaliman <92843271+jemrelmangaliman@users.noreply.github.com>
2023-05-27 16:56:30 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,90 +5,95 @@ var typeFunction = new Typed ("#typing-text", {
     loop: true
 });
 
+ //navbar scroll effect 
+ var navbar = document.querySelector('.navbar');
+ var homelink = document.getElementById('home-link');
+ var aboutlink = document.getElementById('about-link');
+ var projectlink = document.getElementById('project-link');
+ var contactlink = document.getElementById('contact-link');
+ 
+ window.addEventListener('scroll', () => {
+     if (window.scrollY > 70 ) {
+         navbar.classList.add('navbar-scrolled');
+     }
+     else {
+         navbar.classList.remove('navbar-scrolled');
+     }
+ 
+     if (window.scrollY >= 0 && window.scrollY <= 280 ) {
+         homelink.classList.add('link-active');
+     }
+     else {
+         homelink.classList.remove('link-active');
+     }
+ 
+     if (window.scrollY >= 281 && window.scrollY <= 800) {
+         aboutlink.classList.add('link-active');
+     }
+     else {
+         aboutlink.classList.remove('link-active');
+     }
+ 
+     if (window.scrollY >= 801 && window.scrollY <=  1600) {
+         projectlink.classList.add('link-active');
+     }
+     else {
+         projectlink.classList.remove('link-active');
+     }
+     if (window.scrollY >= 1601) {
+         contactlink.classList.add('link-active');
+     }
+     else {
+         contactlink.classList.remove('link-active');
+     }
+ });
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 1 && window.scrollY < 399 ) {
+        var homelink = document.getElementById('home-link');
+        homelink.classList.add('link-active');
+    }
+    else {
+        homelink.classList.remove('link-active');
+    }
+
+    if (window.scrollY > 400 ) {
+        var homelink = document.getElementById('about-link');
+        homelink.classList.add('link-active');
+    }
+    else {
+        homelink.classList.remove('link-active');
+    }
+});
+
 function highlight (val) {
-    if (val == "home") {
-        var homelink = document.getElementById('home-link');
-        homelink.style.borderBottom = "2px solid white";
-        homelink.style.fontWeight = "bold";
-        homelink.style.color = "white";
-        
-        var aboutlink = document.getElementById('about-link');
-        aboutlink.style.borderBottom = "2px solid transparent";
-        aboutlink.style.fontWeight = "normal";
-        aboutlink.style.color = "#98c2d7";
-        
-        var projectlink = document.getElementById('project-link');
-        projectlink.style.borderBottom = "2px solid transparent";
-        projectlink.style.fontWeight = "normal";
-        projectlink.style.color = "#98c2d7";
-
-        var contactlink = document.getElementById('contact-link');
-        contactlink.style.borderBottom = "2px solid transparent";
-        contactlink.style.fontWeight = "normal";
-        contactlink.style.color = "#98c2d7";  
+    var homelink = document.getElementById('home-link');
+    var aboutlink = document.getElementById('about-link');
+    var projectlink = document.getElementById('project-link');
+    var contactlink = document.getElementById('contact-link');
+    if (val == "home") {  
+        homelink.classList.add('link-active');
     }
-    else if (val == "about") {
-        var aboutlink = document.getElementById('about-link');
-        aboutlink.style.borderBottom = "2px solid white";
-        aboutlink.style.fontWeight = "bold";
-        aboutlink.style.color = "white"; 
-        
-        var homelink = document.getElementById('home-link');
-        homelink.style.borderBottom = "2px solid transparent";
-        homelink.style.fontWeight = "normal";
-        homelink.style.color = "#98c2d7";
-        
-        var projectlink = document.getElementById('project-link');
-        projectlink.style.borderBottom = "2px solid transparent";
-        projectlink.style.fontWeight = "normal";
-        projectlink.style.color = "#98c2d7";
-
-        var contactlink = document.getElementById('contact-link');
-        contactlink.style.borderBottom = "2px solid transparent";
-        contactlink.style.fontWeight = "normal";
-        contactlink.style.color = "#98c2d7";
+    else {
+        homelink.classList.remove('link-active');
     }
-    else if (val == "project") {
-        var projectlink = document.getElementById('project-link');
-        projectlink.style.borderBottom = "2px solid white";
-        projectlink.style.fontWeight = "bold";
-        projectlink.style.color = "white"; 
-        
-        var homelink = document.getElementById('home-link');
-        homelink.style.borderBottom = "2px solid transparent";
-        homelink.style.fontWeight = "normal";
-        homelink.style.color = "#98c2d7";
-        
-        var aboutlink = document.getElementById('about-link');
-        aboutlink.style.borderBottom = "2px solid transparent";
-        aboutlink.style.fontWeight = "normal";
-        aboutlink.style.color = "#98c2d7";
-
-        var contactlink = document.getElementById('contact-link');
-        contactlink.style.borderBottom = "2px solid transparent";
-        contactlink.style.fontWeight = "normal";
-        contactlink.style.color = "#98c2d7";
+    if (val == "about") {
+        aboutlink.classList.add('link-active');
     }
-    else if (val == "contact") {
-        var contactlink = document.getElementById('contact-link');
-        contactlink.style.borderBottom = "2px solid white";
-        contactlink.style.fontWeight = "bold";
-        contactlink.style.color = "white";    
-
-        var homelink = document.getElementById('home-link');
-        homelink.style.borderBottom = "2px solid transparent";
-        homelink.style.fontWeight = "normal";
-        homelink.style.color = "#98c2d7";
-        
-        var aboutlink = document.getElementById('about-link');
-        aboutlink.style.borderBottom = "2px solid transparent";
-        aboutlink.style.fontWeight = "normal";
-        aboutlink.style.color = "#98c2d7";
-        
-        var projectlink = document.getElementById('project-link');
-        projectlink.style.borderBottom = "2px solid transparent";
-        projectlink.style.fontWeight = "normal";
-        projectlink.style.color = "#98c2d7";
+    else {
+        aboutlink.classList.remove('link-active');
+    }
+    if (val == "project") {
+        projectlink.classList.add('link-active'); 
+    }
+    else {
+        projectlink.classList.remove('link-active');
+    }
+    if (val == "contact") {
+        contactlink.classList.add('link-active');
+    }
+    else {
+        contactlink.classList.remove('link-active');
     }
 }
 
